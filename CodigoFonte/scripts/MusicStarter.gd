88eb004e355f32458ea7128b9f7aad03868dea16
Filipe Affonso <161388271+filiwili@users.noreply.github.com>
2025-11-29 14:11:51 -0300

    Update MusicStarter.gd

--- CodigoFonte/scripts/MusicStarter.gd
+++ CodigoFonte/scripts/MusicStarter.gd
@@ -5,10 +5,10 @@ func _ready():
 	# Esperar o SettingsManager estar pronto
 	await get_tree().create_timer(2.0).timeout
 	
-	print("🎵 MusicStarter: Iniciando música de fundo...")
+	print(" MusicStarter: Iniciando música de fundo...")
 	
 	# Verificar se o SettingsManager está carregado
 	if has_node("/root/SettingsManager"):
 		SettingsManager.play_background_music()
 	else:
-		print("❌ SettingsManager não encontrado")
+		print(" SettingsManager não encontrado")
